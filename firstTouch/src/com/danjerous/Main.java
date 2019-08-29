808aca125a0c89f8d28ae938fffde7b0b1c479eb
DanierJ <danierjavid@gmail.com>
2019-08-29 11:11:17 -0500

    Primero crea el objeto del driver del browser escogido

---
+++ firstTouch/src/com/danjerous/Main.java
@@ -0,0 +1,18 @@
+package com.danjerous;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        // First thing first, we need to create the driver object before to do anything.
+
+        ChromeDriver chromeDriver = new ChromeDriver();
+        
+
+        
+
+    }
+}
