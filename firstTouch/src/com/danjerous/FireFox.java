9d40be8ee10dfb626ba0b015b7bb3b3f391c5753
DanierJ <danierjavid@gmail.com>
2019-08-29 12:21:01 -0500

    Trabajando con IE

--- firstTouch/src/com/danjerous/FireFox.java
+++ firstTouch/src/com/danjerous/FireFox.java
@@ -2,9 +2,11 @@ package com.danjerous;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 
 public class FireFox {
     private static final String FIREFOX_DRIVER_EXEC = "C:\\Users\\Danier Javid\\Documents\\libs\\selenium-java-3.141.59\\geckodriver.exe";
+
     public static void main(String[] args) {
         //// FIREFOX
         System.setProperty("webdriver.gecko.driver", FIREFOX_DRIVER_EXEC);
@@ -15,8 +17,5 @@ public class FireFox {
         firefoxDriver.get("http://www.google.com");
 
         System.out.println(firefoxDriver.getPageSource());
-
-        
-
     }
 }
---
+++ firstTouch/src/com/danjerous/IE.java
@@ -0,0 +1,22 @@
+package com.danjerous;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+public class IE {
+
+    private static final String IE_DRIVER_EXEC = "C:\\Users\\Danier Javid\\Documents\\libs\\selenium-java-3.141.59\\microsoftWebDriver.exe";
+
+    public static void main(String[] args) {
+        System.setProperty("webdriver.ie.driver", IE_DRIVER_EXEC);
+
+        WebDriver IEDriver = new
+                InternetExplorerDriver();
+
+        IEDriver.get("http://www.google.com");
+
+        System.out.println(IEDriver.getCurrentUrl() );
+
+        // DISM.exe /Online /Add-Capability /CapabilityName:Microsoft.WebDriver~~~~0.0.1.0 para tener el driver 
+    }
+}
