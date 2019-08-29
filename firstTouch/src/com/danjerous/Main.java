9eb2cdcc9de6f24ac16197cdc4e4a25788b52c35
DanierJ <danierjavid@gmail.com>
2019-08-29 11:23:11 -0500

    Pero antes de usarlo hay que proveer el exec del driver

--- firstTouch/src/com/danjerous/Main.java
+++ firstTouch/src/com/danjerous/Main.java
@@ -1,16 +1,24 @@
 package com.danjerous;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Main {
 
+    private static final String EXEC_CHROME_DRIVER  = "C:\\Users\\Danier Javid\\Documents\\libs\\selenium-java-3.141.59";
+
     public static void main(String[] args) {
 	// write your code here
 
-        // First thing first, we need to create the driver object before to do anything.
+        // First thing first, we need to create and set the web browser driver object before to do anything.
 
-        ChromeDriver chromeDriver = new ChromeDriver();
-        
+        System.setProperty("webdriver.chrome.driver", EXEC_CHROME_DRIVER);
+
+        WebDriver chromeDriver = new ChromeDriver();
+
+
+
+
 
         
 
