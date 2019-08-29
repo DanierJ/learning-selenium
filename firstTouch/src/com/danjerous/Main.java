6ce56c7e7845153c3cc18606dea4c3898732c7cd
DanierJ <danierjavid@gmail.com>
2019-08-29 11:45:50 -0500

    Invocando el navegador con la url especificada y obteniendo información de ello

--- firstTouch/src/com/danjerous/Main.java
+++ firstTouch/src/com/danjerous/Main.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Main {
 
-    private static final String EXEC_CHROME_DRIVER  = "C:\\Users\\Danier Javid\\Documents\\libs\\selenium-java-3.141.59";
+    private static final String EXEC_CHROME_DRIVER  = "C:\\Users\\Danier Javid\\Documents\\libs\\selenium-java-3.141.59\\chromedriver.exe";
 
     public static void main(String[] args) {
 	// write your code here
@@ -13,8 +13,14 @@ public class Main {
         // First thing first, we need to create and set the web browser driver object before to do anything.
 
         System.setProperty("webdriver.chrome.driver", EXEC_CHROME_DRIVER);
-
         WebDriver chromeDriver = new ChromeDriver();
+
+        // Invoking web browser at the specified url
+        chromeDriver.get("http://www.google.com");
+
+        // Getting some data from the page.
+        System.out.println( chromeDriver.getTitle());
+
 
 
 
