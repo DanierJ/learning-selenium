8e4efac29d34fa36385e4425dc6c7f7d1c978cad
DanierJ <danierjavid@gmail.com>
2019-09-02 18:17:01 -0500

    Syncronitation - Explicit wait

--- firstTouch/src/com/danjerous/ImplicitWait.java
+++ firstTouch/src/com/danjerous/ImplicitWait.java
@@ -4,6 +4,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
 
@@ -13,28 +16,57 @@ public class ImplicitWait {
     static {
         Browser.CHROME.setSystemProperty();
         driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+     //   driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
     public static void main(String[] args) {
-     driver.get("https://alaskatrips.poweredbygps.com/");
-     driver.findElement(By.cssSelector("label[for='hotel-car-package-type-hp-package']")).click();
+       // implicitWait();
+        explicitWait();
+    }
 
-     driver.findElement(By.id("hotel-destination-hp-package")).click();
-     driver.findElement(By.id("hotel-destination-hp-package")).sendKeys("nyc");
+    public static void implicitWait() {
+        driver.get("https://alaskatrips.poweredbygps.com/");
+        driver.findElement(By.cssSelector("label[for='hotel-car-package-type-hp-package']")).click();
 
-     driver.findElement(By.cssSelector("label[for='package-hc-departing-hp-package']")).click();
-     driver.findElement(By.cssSelector("button[data-month='8'][data-day='9']")).click();
+        driver.findElement(By.id("hotel-destination-hp-package")).click();
+        driver.findElement(By.id("hotel-destination-hp-package")).sendKeys("nyc");
 
-     driver.findElement(By.cssSelector("label[for='package-hc-returning-hp-package']")).click();
-     driver.findElement(By.cssSelector("button[data-month='8'][data-day='12']")).click();
+        driver.findElement(By.cssSelector("label[for='package-hc-departing-hp-package']")).click();
+        driver.findElement(By.cssSelector("button[data-month='8'][data-day='9']")).click();
 
-     driver.findElement(By.id("package-hc-departing-hp-package")).sendKeys(Keys.ENTER);
+        driver.findElement(By.cssSelector("label[for='package-hc-returning-hp-package']")).click();
+        driver.findElement(By.cssSelector("button[data-month='8'][data-day='12']")).click();
 
-     driver.findElement(By.xpath("//a[contains(text(),'New York, NY, United States (JFK-John F. Kennedy I')]")).click();
+        driver.findElement(By.id("package-hc-departing-hp-package")).sendKeys(Keys.ENTER);
+
+        driver.findElement(By.xpath("//a[contains(text(),'New York, NY, United States (JFK-John F. Kennedy I')]")).click();
 
 
-     driver.findElement(By.cssSelector("a[target='17117062']")).click();
+        driver.findElement(By.cssSelector("a[target='17117062']")).click();
+    }
+    public static void explicitWait() {
+        driver.get("https://alaskatrips.poweredbygps.com/");
+        driver.findElement(By.cssSelector("label[for='hotel-car-package-type-hp-package']")).click();
+
+        driver.findElement(By.id("hotel-destination-hp-package")).click();
+        driver.findElement(By.id("hotel-destination-hp-package")).sendKeys("nyc");
+
+        driver.findElement(By.cssSelector("label[for='package-hc-departing-hp-package']")).click();
+        driver.findElement(By.cssSelector("button[data-month='8'][data-day='9']")).click();
+
+        driver.findElement(By.cssSelector("label[for='package-hc-returning-hp-package']")).click();
+        driver.findElement(By.cssSelector("button[data-month='8'][data-day='12']")).click();
+
+        driver.findElement(By.id("package-hc-departing-hp-package")).sendKeys(Keys.ENTER);
+
+        driver.findElement(By.xpath("//a[contains(text(),'New York, NY, United States (JFK-John F. Kennedy I')]")).click();
+
+        WebDriverWait delayedDriver = new WebDriverWait(driver,20);
+        delayedDriver.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("a[target='17117062']")));
+
+        driver.findElement(By.cssSelector("a[target='17117062']")).click();
 
     }
+
+
 }
