3e04c4d7671a196e10962f6f2ab02dcffba5dc62
DanierJ <danierjavid@gmail.com>
2019-09-02 21:31:05 -0500

    Working with Actions - onMouseOver

---
+++ firstTouch/src/com/danjerous/ActionsDemo.java
@@ -0,0 +1,29 @@
+package com.danjerous;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class ActionsDemo {
+    private static final WebDriver driver;
+
+    static {
+        Browser.CHROME.setSystemProperty();
+        driver = new ChromeDriver();
+
+    }
+    public static void main(String[] args) {
+        onMouseOver();
+    }
+
+    public static void onMouseOver() {
+        driver.get("https://www.amazon.com/");
+        Actions actions = new Actions(driver);
+
+        actions
+               .moveToElement(driver.findElement(By.id("nav-link-accountList")))
+               .build()
+               .perform();
+    }
+}
