4bdf7e68448f1c3465d14185e6c1e9fb1aa4cf71
DanierJ <danierjavid@gmail.com>
2019-09-03 11:58:26 -0500

    Real time exercises (e2e programmming) - scope and getting info dynamically

--- firstTouch/src/com/danjerous/Scope.java
+++ firstTouch/src/com/danjerous/Scope.java
@@ -1,9 +1,16 @@
 package com.danjerous;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
 
 public class Scope {
     private static final WebDriver driver;
@@ -15,11 +22,15 @@ public class Scope {
     }
 
     public static void main(String[] args) {
-        countingLinks();
+       countingLinks();
 
-        countingFooterLinks();
+       countingFooterLinks();
+
+       countingFooterFirstColumnLinks();
     }
 
+
+    // Scope
     public static void countingLinks() {
         driver.get("http://www.qaclickacademy.com/practice.php");
 
@@ -28,10 +39,10 @@ public class Scope {
         System.out.println(count);
     }
 
+    // Scope
     public static void countingFooterLinks() {
-        driver.get("http://www.qaclickacademy.com/practice.php");
 
-        // Limiting the scope to a particular section. 
+        // Limiting the scope to a particular section.
         WebElement footerDriver = driver.findElement(By.id("gf-BIG"));
 
         int count = footerDriver.findElements(By.tagName("a")).size();
@@ -41,5 +52,74 @@ public class Scope {
 
         System.out.println(count);*/
 
+        System.out.println(count);
     }
+
+    // scope
+    public static void countingFooterFirstColumnLinks() {
+
+
+        WebElement footerDriver = driver.findElement(By.id("gf-BIG"));
+
+        WebElement footerFirstColumn = footerDriver.findElement(By.xpath("//table/tbody/tr/td[1]/ul"));
+
+        int count = footerFirstColumn.findElements(By.tagName("a")).size();
+
+        System.out.println(count);
+
+        List<WebElement> footerFirstColumnLinks = footerFirstColumn.findElements(By.tagName("a"));
+
+        multiClick(footerFirstColumnLinks);
+
+        getWindowsTitle();
+
+    }
+
+    // Opening multiple links on different tabs
+    public static void multiClick (List<WebElement> webElementList) {
+       // webElementList.forEach(WebElement::click);
+
+        Actions actions = new Actions(driver);
+
+       /* for (int i = 0; i < webElementList.size(); i++) {
+            actions
+                    .moveToElement(webElementList.get(i))
+                    .keyDown(Keys.CONTROL)
+                    .click()
+                    .build()
+                    .perform();
+        }
+
+        for (WebElement webElement : webElementList) {
+            actions.moveToElement(webElement)
+                    .keyDown(Keys.CONTROL)
+                    .click()
+                    .build()
+                    .perform();
+        }
+        */
+       /* webElementList.forEach(webElement -> {
+            actions.moveToElement(webElement)
+                    .keyDown(Keys.CONTROL)
+                    .click()
+                    .build()
+                    .perform();
+        });*/
+        webElementList.forEach(webElement -> {
+           String clickOnLink = Keys.chord(Keys.CONTROL, Keys.ENTER);
+
+           webElement.sendKeys(clickOnLink);
+        });
+    }
+
+    // Getting dynamically the title of all the open tabs.
+    public static void getWindowsTitle() {
+        Set<String> ids = driver.getWindowHandles();
+
+        ids.forEach(id -> {
+            driver.switchTo().window(id);
+            System.out.println(driver.getTitle());
+        });
+    }
+
 }
