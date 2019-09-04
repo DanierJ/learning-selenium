8416ed367b92e8ab027fbef7961bdb4406b99255
DanierJ <danierjavid@gmail.com>
2019-09-04 18:07:29 -0500

    TESTNG - working with the xml file to execute all the tests in a suite

---
+++ firstTouch/src/com/danjerous/testng/BasicsTNGG.java
@@ -0,0 +1,11 @@
+package com.danjerous.testng;
+
+import org.testng.annotations.Test;
+
+public class BasicsTNGG {
+
+    @Test
+    public void testFromAnotherClass() {
+        System.out.println("Hellllloooooo.");
+    }
+}
