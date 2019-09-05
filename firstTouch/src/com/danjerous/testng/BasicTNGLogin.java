81c72db8c21ea59ad3735f09a883302ec0d850e7
DanierJ <danierjavid@gmail.com>
2019-09-05 10:39:58 -0500

    TESTNG - Controling what test modules are executed

---
+++ firstTouch/src/com/danjerous/testng/BasicTNGLogin.java
@@ -0,0 +1,25 @@
+package com.danjerous.testng;
+
+import org.testng.annotations.Test;
+
+public class BasicTNGLogin {
+
+    @Test
+    public void webLoginCarLoan() {
+        // Selenium
+        System.out.println("Web login car");
+    }
+
+    @Test
+    public void mobileLoginCarLoan() {
+        // Appium
+        System.out.println("Mobile login car");
+
+    }
+
+    @Test
+    public void loginAPICarLoan() {
+        // REST API automation
+        System.out.println("Login car rest api ");
+    }
+}
---
+++ firstTouch/src/com/danjerous/testng/BasicTNGLoginHome.java
@@ -0,0 +1,24 @@
+package com.danjerous.testng;
+
+import org.testng.annotations.Test;
+
+public class BasicTNGLoginHome {
+    @Test
+    public void webLoginHomeLoan() {
+        // Selenium
+        System.out.println("Web login home");
+    }
+
+    @Test
+    public void mobileLoginHomeLoan() {
+        // Appium
+        System.out.println("Mobile login home");
+
+    }
+
+    @Test
+    public void loginAPIHomeLoan() {
+        // REST API automation
+        System.out.println("Login home rest api ");
+    }
+}
