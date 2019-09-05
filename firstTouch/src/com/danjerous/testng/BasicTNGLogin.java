f50dbb6bc7fc8f831d186b2ce121dcd04ef0a1cf
DanierJ <danierjavid@gmail.com>
2019-09-05 11:28:47 -0500

    TESTNG - excluding methods from being executed with regex and executing tests at a package level

--- firstTouch/src/com/danjerous/testng/BasicTNGLogin.java
+++ firstTouch/src/com/danjerous/testng/BasicTNGLogin.java
@@ -18,6 +18,19 @@ public class BasicTNGLogin {
     }
 
     @Test
+    public void mobilePayment() {
+        // Appium
+        System.out.println("Mobile payment car");
+
+    }
+    @Test
+    public void mobileChar() {
+        // Appium
+        System.out.println("Mobile char car");
+
+    }
+
+    @Test
     public void loginAPICarLoan() {
         // REST API automation
         System.out.println("Login car rest api ");
