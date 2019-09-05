b83722f676df349f51f3d1ad186199d10cf7fbed
DanierJ <danierjavid@gmail.com>
2019-09-05 18:13:23 -0500

    TESTNG - Running test cases in parallel and generating reports

--- firstTouch/src/com/danjerous/testng/Listeners.java
+++ firstTouch/src/com/danjerous/testng/Listeners.java
@@ -8,6 +8,6 @@ public class Listeners implements ITestListener {
     @Override
     public void onTestSuccess(ITestResult result) {
         System.out.println("Yeah! I succeed: ");
-        System.out.println("---[" + result.getStatus() + "]---");
+        System.out.println("---[" + result.getName() + "]---");
     }
 }
