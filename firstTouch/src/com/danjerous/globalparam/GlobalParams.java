f6d68f2525bdd90ed7be0504cf8d765f629f6112
DanierJ <danierjavid@gmail.com>
2019-09-06 11:45:16 -0500

    GEV - setting browser conditions dynamically with global variables

--- firstTouch/src/com/danjerous/globalparam/GlobalParams.java
+++ firstTouch/src/com/danjerous/globalparam/GlobalParams.java
@@ -1,5 +1,9 @@
 package com.danjerous.globalparam;
 
+import com.danjerous.Browser;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
@@ -11,19 +15,21 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class GlobalParams {
-    @BeforeSuite
+   private WebDriver driver;
+
+    @Test
     public void Login() throws IOException {
 
         Properties properties = new Properties();
         FileInputStream fileInputStream = new FileInputStream(new File("C:\\Users\\Danier Javid\\Desktop\\Courses\\selenium\\firstTouch\\src\\com\\danjerous\\globalparam\\datadriver.properties")); // To get the input file
 
         properties.load(fileInputStream);
-
-
-
         System.out.println("Username: " + properties.getProperty("username"));
-
         // driver.findElement(By.xpath("")).sendKeys(username);
+
+        setDriver(properties.getProperty("browser"));
+
+        driver.get(properties.getProperty("url"));
     }
 
     @Test
@@ -34,5 +40,19 @@ public class GlobalParams {
     @AfterSuite
     public void uninstallSoftware() {
         System.out.println("I'm the Last");
+    }
+
+
+    public void setDriver(String browser) {
+
+        if (browser.equalsIgnoreCase(Browser.CHROME.toString())) {
+            Browser.CHROME.setSystemProperty();
+            driver = new ChromeDriver();
+
+        } else if (browser.equalsIgnoreCase(Browser.FIREFOX.toString())) {
+
+            Browser.FIREFOX.setSystemProperty();
+            driver = new FirefoxDriver();
+        }
     }
 }
