c9d653b8c6be35fadc8399be59a2bd1800df8241
DanierJ <danierjavid@gmail.com>
2019-09-09 21:38:28 -0500

    FINAL FRAMEWORK - crearing base setup class

---
+++ testingFramework/src/main/java/com/danjerous/framework/Base.java
@@ -0,0 +1,46 @@
+package com.danjerous.framework;
+
+import com.danjerous.Browser;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class Base {
+
+    private WebDriver driver;
+
+    public WebDriver initializeDriver() throws IOException {
+        Properties properties = new Properties();
+
+        FileInputStream fileInputStream = new FileInputStream(new File("C:\\Users\\Danier Javid\\Desktop\\Courses\\selenium\\testingFramework\\data.properties"));
+        properties.load(fileInputStream);
+
+       // setDriver(properties.getProperty("browser"));
+
+        return setDriver(properties.getProperty("browser"));
+
+    }
+
+    private WebDriver setDriver(String browser) {
+
+        if (browser.equalsIgnoreCase(Browser.CHROME.toString())) {
+            Browser.CHROME.setSystemProperty();
+            driver = new ChromeDriver();
+
+        } else if (browser.equalsIgnoreCase(Browser.FIREFOX.toString())) {
+
+            Browser.FIREFOX.setSystemProperty();
+            driver = new FirefoxDriver();
+        }
+
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        return driver;
+    }
+}
--- testingFramework/src/test/java/com/danjerous/framework/AppTest.java
+++ testingFramework/src/test/java/com/danjerous/framework/AppTest.java
@@ -1,20 +1,24 @@
 package com.danjerous.framework;
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+
+import java.io.IOException;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
+public class AppTest  extends Base
 {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWithTrue() throws IOException {
+        WebDriver driver = initializeDriver();
+        driver.get("http://www.google.com");
+
+        Assert.assertEquals(driver.getTitle(), "Google");
     }
 }
