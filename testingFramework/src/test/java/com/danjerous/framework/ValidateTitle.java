e1d6fc41040acd1940b4d9099090ca6969ade14b
DanierJ <danierjavid@gmail.com>
2019-09-12 11:45:06 -0500

    FINAL FRAMEWORK - integrating log4j

--- testingFramework/src/test/java/com/danjerous/framework/ValidateTitle.java
+++ testingFramework/src/test/java/com/danjerous/framework/ValidateTitle.java
@@ -1,6 +1,8 @@
 package com.danjerous.framework;
 
 import com.danjerous.framework.page_objects.LandingPage;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -12,10 +14,15 @@ import static org.testng.Assert.assertEquals;
 public class ValidateTitle extends Base {
     private WebDriver driver;
 
+    private static Logger log = LogManager.getLogger(Base.class.getName());
+
     @BeforeTest
     public void init() throws IOException {
         driver = initializeDriver();
+        log.info("Driver is initialized.");
+
         driver.get(getProperty("url"));
+        log.info("Navigate to Home page.");
     }
 
     @Test
@@ -23,5 +30,6 @@ public class ValidateTitle extends Base {
         LandingPage landingPage = new LandingPage(driver);
 
         assertEquals(landingPage.getTitle().getText(), "FEATURED COURSES");
+        log.info("Successfully validated");
     }
 }
