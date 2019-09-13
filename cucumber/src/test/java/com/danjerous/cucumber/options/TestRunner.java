bca3cf4987ee648838a8a02901930688669f9f21
DanierJ <danierjavid@gmail.com>
2019-09-13 12:35:17 -0500

    CUCUMBER - writing test runner o trigger the tests

---
+++ cucumber/src/test/java/com/danjerous/cucumber/options/TestRunner.java
@@ -0,0 +1,11 @@
+package com.danjerous.cucumber.options;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/java/features", glue = "stepdefinitions")
+public class TestRunner {
+
+}
--- cucumber/src/test/java/stepdefinitions/stepDefinition.java
+++ cucumber/src/test/java/stepdefinitions/stepDefinition.java
@@ -1,4 +1,4 @@
-package com.danjerous.cucumber.stepdefinitions;
+package stepdefinitions;
 
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
